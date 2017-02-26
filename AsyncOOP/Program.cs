bfd9dffd8f6ae296264940d9cac1cb92537631e0
Connor Goddard <hello@connorlukegoddard.com>
2017-02-26 15:22:22 +0000

    Initial commit.

---
+++ AsyncOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Watcher watcher = Watcher.CreateAsync().GetAwaiter().GetResult();
+
+            Watcher watcher = Watcher.CreateAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine(watcher.Bytes);
+
+            Console.WriteLine(watcher.Content);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ AsyncOOP/Watcher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncOOP
+{
+    class Watcher : IWatcher
+    {
+        public int Bytes { get; set; }
+        public string Content { get; set; }
+        public string Content2 { get; set; }
+
+        private Watcher()
+        {
+        }
+
+        private async Task<Watcher> InitializeAsync()
+        {
+            Bytes = await CountBytesAsync("http://bbc.co.uk");
+            Content = await GetContentAsync("http://bbc.co.uk/");
+            Content2 = GetContent("http://bbc.co.uk");
+
+            return this;
+        }
+
+        public static Task<Watcher> CreateAsync()
+        {
+            var result = new Watcher();
+            return result.InitializeAsync();
+        }
+
+        public async Task<int> CountBytesAsync(string url)
+        {
+            var client = new HttpClient();
+            var bytes = await client.GetByteArrayAsync(url);
+            return bytes.Length;
+        }
+
+        public async Task<string> GetContentAsync(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+
+            return string.Empty;
+        }
+
+        public string GetContent(string url)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync(url).GetAwaiter().GetResult();
+
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+
+            return string.Empty;
+        }
+    }
+}
